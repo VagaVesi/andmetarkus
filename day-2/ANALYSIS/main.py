130c78a5388b0ac6168c5b65195645bff847feaf
Kaido Vetevoog <kaido.vetevoog@gmail.com>
2025-09-08 10:39:07 +0300

    ex1.py

--- day-2/ANALYSIS/main.py
+++ day-2/ANALYSIS/main.py
@@ -1,7 +1,27 @@
-def main():
-    print("Hello, Andmetarkus 2025!")
+from file_functions import FileFunctions
 
-# Siia hakkame lisama koodi, millega testime loodud funktsioone.
+
+def main():
+
+    file_path = r"/Users/vaga_vesi/Projects/andmetarkus2025/andmetarkus/day-2/ANALYSIS/request-log.txt"
+
+    # filecontent = ""
+
+    # with open(file_path, encoding='utf-8') as f:
+    #     filecontent = f.read()
+
+    # request_log_entries = filecontent.split("\n")
+
+    request_log_entries = FileFunctions().read_file(file_path)
+
+    for line in request_log_entries:
+        print(line + "\n")
+
+    row_count = len(request_log_entries)
+    print(f"Logifailis on {row_count} rida.")
+
+    FileFunctions().add_line_to_file(
+        file_path, f"Logifailis on {row_count} rida.")
 
 
 if __name__ == "__main__":
