4d9497f0e71c88bbe03e200c23e5d2b5b64aa5fd
Kaido Vetevoog <kaido.vetevoog@gmail.com>
2025-09-08 08:47:16 +0300

    fix

--- day-2/ANALYSIS/main.py
+++ day-2/ANALYSIS/main.py
@@ -1,8 +1,7 @@
 def main():
     print("Hello, Andmetarkus 2025!")
 
-
-# Siia hakkame lisama koodi, mida testime
+# Siia hakkame lisama koodi, millega testime loodud funktsioone.
 
 
 if __name__ == "__main__":
