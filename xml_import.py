b6b1afa4aabb40fafbdcffe21efe97586a14cd18
Kaido Vetevoog <kaido.vetevoog@gmail.com>
2025-09-09 16:24:37 +0300

    xml import

--- xml_import.py
+++ xml_import.py
@@ -9,4 +9,13 @@ xml_content = response.content
 # Proovi leida sobiv XPath, nt kõik <place> elemendid
 df = pd.read_xml(xml_content, xpath=".//place")
 
-print(df.head())
+min_temp_row = df[df['tempmin'] == df['tempmin'].min()]
+max_temp_row = df[df['tempmin'] == df['tempmin'].max()]
+
+# Rühmitame temperatuuriskaala järgi (nt tempmin)
+# Näiteks: mitu linna igas tempmin väärtuses
+grouped = df.groupby("tempmin").size().reset_index(name="count")
+
+print(min_temp_row)
+print(max_temp_row)
+print(grouped)
