b2027bfacc15bf646b294ad17636c0534a548607
Kaido Vetevoog <kaido.vetevoog@gmail.com>
2025-09-09 12:06:27 +0300

    timestamp deleted

--- day-3/andmetootlus.py
+++ day-3/andmetootlus.py
@@ -3,4 +3,8 @@ import pandas as pd
 
 df = pd.read_json('day-3/elektrihind/el_data_2025.json')
 df_expanded = pd.json_normalize(df["data"])
+
+if "timestamp" in df_expanded.columns:
+    df_expanded = df_expanded.drop(columns=["timestamp"])
+
 print(df_expanded)
