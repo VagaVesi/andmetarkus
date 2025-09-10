599d7003ed140a1e8a7864e3124ff08d2f0ae44c
Kaido Vetevoog <kaido.vetevoog@gmail.com>
2025-09-10 11:32:58 +0300

    täiendatud ex5 esimene osa

--- day-4/ex5.py
+++ day-4/ex5.py
@@ -3,13 +3,15 @@ riigid = ["Eesti", "Austria", "Belgia", "Andorra",
 
 # Leia ja salvesta eraldi järjendisse kõik riigid, mis algavad A ja E tähega. Prindi uus nimek terminali.
 
+OTSITAVAD_ALGUSTÄHED = ("A", "E")
+
 # list comprehension
-riigid_ae = [riik for riik in riigid if riik.startswith(("A", "E"))]
+riigid_ae = [riik for riik in riigid if riik.startswith(OTSITAVAD_ALGUSTÄHED)]
 
 # for tsükkel
 riigid_ae2 = []
 for riik in riigid:
-    if riik[0] in ("A", "E"):
+    if riik[0] in OTSITAVAD_ALGUSTÄHED:
         riigid_ae2.append(riik)
 
 
@@ -18,6 +20,15 @@ riigid_ae3 = []
 for riik in riigid:
     if riik.startswith("A") or riik.startswith("E"):
         riigid_ae3.append(riik)
+
+# for eraldi if laused, samaväärne eelmisega
+
+riigid_ae4 = []
+for riik in riigid:
+    if riik.startswith("A"):
+        riigid_ae4.append(riik)
+    elif riik.startswith("E"):
+        riigid_ae4.append(riik)
 
 # testime lahendusi
 print(riigid_ae)
